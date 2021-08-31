39eda3cdf0058e4f9a5d96b1321f85b389ba1d91
Hitesh Ray <ray.hitesh@gmail.com>
2021-08-31 17:38:29 +0530

    Automation Script for Task2

--- automation.sh
+++ automation.sh
@@ -35,3 +35,49 @@ tar cvf /tmp/hitesh-httpd-log-$d.tar  /var/log/apache2/*.log
 s3_bucket=upgrad-hitesh
 
 aws s3 cp /tmp/hitesh-httpd-log-$d.tar s3://$s3_bucket/hitesh-httpd-log-$d.tar
+
+log_type=$( ls -lah /tmp/hitesh-httpd-log-$d.tar|awk '{ print $9 }'|cut -c 13-21 )
+date_created=$( ls -lah /tmp/hitesh-httpd-log-$d.tar|awk '{ print $9 }'|cut -c 23-37 )
+file_type=$( ls -lah /tmp/hitesh-httpd-log-$d.tar|awk '{ print $9 }'|cut -c 39- )
+file_size=$( ls -lah /tmp/hitesh-httpd-log-$d.tar|awk '{ print $5 }' )
+
+ls /var/www/html/inventory.html &>/dev/null
+if [ $? -eq 0 ];
+then
+        echo "inventory.html file already exist......"
+        echo "updating inventory.html with new archive file info....."
+        sed -i -e '\@</body>@i\<tr><td>'$log_type'</td><td>'$date_created'</td><td>'$file_type'</td><td>'$file_size'</td></tr>' /var/www/html/inventory.html
+else
+        echo "inventory.html does not exist....."
+        echo "creating inventory.html file....."
+        {
+        echo '<!DOCTYPE html>'
+        echo '<html>'
+        echo '<style>'
+        echo 'table, th, td {border:1px black;}'
+        echo '</style>'
+        echo '<body>'
+        echo '<table style="width:100%">'
+        echo '<tr>'
+        echo '<th>Log Type</th>'
+        echo '<th>Date Created</th>'
+        echo '<th>Type</th>'
+        echo '<th>Size</th>'
+        echo '</tr>'
+        echo '</body>'
+        echo '</html>'
+        }>>/var/www/html/inventory.html
+        sed -i -e '\@</body>@i\<tr><td>'$log_type'</td><td>'$date_created'</td><td>'$file_type'</td><td>'$file_size'</td></tr>' /var/www/html/inventory.html
+fi
+
+ls /etc/cron.d/automation &>/dev/null
+if [ $? -eq 0 ];
+then
+        echo "corn job is scheduled........."
+else
+        echo "cron job not schedule ............."
+        echo "setting up cron job .........."
+
+        echo "00 00 * * * root /root/Automation_Project/automation.sh" >> /etc/cron.d/automation
+        echo "cron job is scheduled for daily starting from tomorrow ........"
+fi
